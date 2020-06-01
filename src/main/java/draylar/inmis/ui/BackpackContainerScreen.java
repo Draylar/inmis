531e66f9905dc939df32f48dfadef04eb40f6c02
Draylar <samuelhimes@gmail.com>
2020-06-01 13:28:40 -0500

    Fix #15 by upgrading Spinnery to 2.0.28 + small gradle refactoring

--- src/main/java/draylar/inmis/ui/BackpackContainerScreen.java
+++ src/main/java/draylar/inmis/ui/BackpackContainerScreen.java
@@ -28,15 +28,15 @@ public class BackpackContainerScreen extends BaseContainerScreen<BackpackContain
             int containerWidth = Math.max(9, backpackItem.getTier().getRowWidth()) * SLOT_SIZE + WIDTH_PADDING;
 
             WInterface mainInterface = getInterface().setBlurred(true).setTheme(Inmis.id("backpack"));
-            WPanel mainPanel = mainInterface.createChild(WPanel.class, Position.of(0, 0, 0), Size.of(containerWidth, INVENTORY_LABEL_EXTRA + backpackItem.getTier().getNumberOfRows() * 18 + 108));
+            WPanel mainPanel = mainInterface.createChild(WPanel::new, Position.of(0, 0, 0), Size.of(containerWidth, INVENTORY_LABEL_EXTRA + backpackItem.getTier().getNumberOfRows() * 18 + 108));
             mainPanel.center();
             mainPanel.setOnAlign(WAbstractWidget::center);
 
-            mainPanel.createChild(WStaticText.class).setText(new TranslatableText("item.inmis." + backpackItem.getTier().getName() + "_backpack")).setPosition(Position.of(mainPanel, 8, 6, 0));
+            mainPanel.createChild(WStaticText::new).setText(new TranslatableText("item.inmis." + backpackItem.getTier().getName() + "_backpack")).setPosition(Position.of(mainPanel, 8, 6, 0));
 
             WSlot.addArray(Position.of(mainPanel, containerWidth / 2 - (SLOT_SIZE * backpackItem.getTier().getRowWidth()) / 2, 18, 1), Size.of(SLOT_SIZE, SLOT_SIZE), mainInterface, 0, BackpackContainer.BACKPACK_INVENTORY, backpackItem.getTier().getRowWidth(), backpackItem.getTier().getNumberOfRows());
             WSlot.addPlayerInventory(Position.of(mainPanel, containerWidth / 2 - (SLOT_SIZE * 9) / 2, INVENTORY_LABEL_EXTRA + backpackItem.getTier().getNumberOfRows() * SLOT_SIZE + TOP_OFFSET, 1), Size.of(18, 18), mainInterface);
-            mainPanel.createChild(WStaticText.class).setText(new TranslatableText("key.categories.inventory")).setPosition(Position.of(mainPanel, containerWidth / 2 - (SLOT_SIZE * 9) / 2, backpackItem.getTier().getNumberOfRows() * SLOT_SIZE + TOP_OFFSET - 4));
+            mainPanel.createChild(WStaticText::new).setText(new TranslatableText("key.categories.inventory")).setPosition(Position.of(mainPanel, containerWidth / 2 - (SLOT_SIZE * 9) / 2, backpackItem.getTier().getNumberOfRows() * SLOT_SIZE + TOP_OFFSET - 4));
         }
     }
 }
